9e4dc909e89497ce902cc583e604c356ee0eb853
nbc7 <nathanborgescastro@gmail.com>
2023-06-03 11:27:26 -0300

    solution

--- productStructure.js
+++ productStructure.js
@@ -1,18 +1,34 @@
 const products = [
-  "preto-PP",
-  "preto-M",
-  "preto-G",
-  "preto-GG",
-  "preto-GG",
-  "branco-PP",
-  "branco-G",
-  "vermelho-M",
-  "azul-XG",
-  "azul-XG",
-  "azul-XG",
-  "azul-P"
-]
+  'preto-PP',
+  'preto-M',
+  'preto-G',
+  'preto-GG',
+  'preto-GG',
+  'branco-PP',
+  'branco-G',
+  'vermelho-M',
+  'azul-XG',
+  'azul-XG',
+  'azul-XG',
+  'azul-P',
+];
 
 module.exports = () => {
-	// Seu código vai aqui!
-}
+  const productVariants = {};
+
+  products.forEach((product) => {
+    const [color, size] = product.split('-');
+
+    if (!productVariants[color]) {
+      productVariants[color] = {};
+    }
+
+    if (!productVariants[color][size]) {
+      productVariants[color][size] = 1;
+    } else {
+      productVariants[color][size]++;
+    }
+  });
+
+  return productVariants;
+};
